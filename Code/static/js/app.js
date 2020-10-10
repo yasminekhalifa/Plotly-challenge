94faee3d605cb08d076aa380c93c8afbef6b22e0
yasminekhalifa <yasminaekhalifa@gmail.com>
2020-10-10 17:20:37 +1100

    bubble chart

--- Code/static/js/app.js
+++ Code/static/js/app.js
@@ -1,10 +1,6 @@
-  // var sample_value = data.names;
-  //   console.log(sample_value)})
+    d3.json("./../../samples.json").then(function(data) {
+      console.log(data);
     function populateFilter() {
-      // Let's populate the <option> elements in 
-      // our <select> from the database. 
-      d3.json("./../../samples.json").then(function(data){
-
         var filerOptions = ["All"];
         filerOptions = filerOptions.concat(data.names);
         console.log(filerOptions);
@@ -18,62 +14,75 @@
         // Bind an event to refresh the data
         // when an option is selected.
         d3.select("#selDataset").on("change", refreshCharts);
-      });
-    }
+
+      };
     populateFilter();
+    function filterIds(selectedId) {
+      return data.samples.find(s => s.id == selectedId)
+};
     function refreshCharts(event) {
       // event.target will refer tp the selector
       // from which we will get the selected option
       var selectedValue = d3.select("#selDataset").property('value');
       console.log(selectedValue);
+      var filteredData = filterIds(selectedValue)
       // With the selectedValue we can refresh the charts
       // filtering if needed. 
-      buildareaBarChart(selectedValue);
-    }
-    // function buildareaBarChart(selectedArea) {
-    //   let xl = [];
-    //   let yl = [];
-    //     for (var i = 0; i < data.samples.length; i++) {
-    //       xl.push(data.samples[i].sample_values)
-    //       yl.push(data.samples[i].durationminutes)
-    //     }
+
+      console.log(filteredData);
     
-    //     let trace = {
-    //       x: xl,
-    //       y: yl,
-    //       marker: {
-    //         color: '#37536D'
-    //       },
-    //       type: 'bar'
-    //     };
-    //     let layout = {
-    //       title: 'Duration per parking space',
-    //       yaxis: { title: 'Sample value' },
-    //       xaxis: { title: 'OTU ID' , type: "category"}
-    //     };
-    //     Plotly.plot('plot', [trace], layout, { displayModeBar: false });
-    
-    //   });
-    // }
-    populateFilter();
-    // buildareaBarChart();
-    //Use `sample_values` as the values for the bar chart.
-  //   const myUsers = [
-  //     { name: 'shark', likes: 'ocean' },
-  //     { name: 'turtle', likes: 'pond' },
-  //     { name: 'otter', likes: 'fish biscuits' }
-  // ]
-  
-  // const usersByLikes = myUsers.map(item => {
-  //     const container = {};
-  
-  //     container[item.name] = item.likes;
-  //     container.age = item.name.length * 10;
-  
-  //     return container;
-  // })
-  
-  // console.log(usersByLikes);
-    // * Use `otu_ids` as the labels for the bar chart.
-    
-    // * Use `otu_labels` as the hovertext for the chart.
+      // Use the map method with the arrow function to return all the filtered movie titles.
+      var bacterias = []
+      for(var i in filteredData.sample_values){
+        bacterias.push(
+          {
+            id:filteredData.otu_ids[i],
+            values: filteredData.sample_values[i],
+            label: filteredData.otu_labels[i]
+          }
+        )
+      }
+      var top_bacteria = bacterias.sort((a,b) => a.values > b.values ).slice(0,10);
+      let x_values = top_bacteria.map(b => b.values);
+      let y_values = top_bacteria.map(b => b.id);
+      let text = top_bacteria.map(b => b.label);
+
+      console.log(x_values);
+
+      var bar = {
+                  x: x_values,     //top 10 values
+                  y: y_values,     // formatted ids
+                  text: text,     //labels
+                  type: "bar", //bar graphs
+                  orientation: "h" //horizontal graphs
+          };
+          var setLayout = {
+                  title: "Bacteria found",
+                  yaxis: { title: 'OTU IDs', type: "category" },
+                  xaxis: { title: 'Sample values' }
+          };
+          var data = [bar];
+          Plotly.newPlot("bar", data, setLayout);
+
+      var trace1 = {
+        x: y_values,
+        y: x_values,
+        mode: 'markers',
+        text: text,
+        marker: {
+          color: [y_values],
+          opacity: [1, 0.8, 0.6, 0.4],
+          size: x_values
+        }
+      };
+      
+      var data = [trace1];
+      
+      var layout = {
+        title: 'Marker Size and Color',
+        showlegend: false,
+      };
+      
+      Plotly.newPlot('bubble', data, layout);
+  };
+  });
